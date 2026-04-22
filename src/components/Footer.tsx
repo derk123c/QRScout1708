9e441f0090e47670695e7849ee95ef5c8958399c
Price <35711526+derk123c@users.noreply.github.com>
2026-04-22 16:31:01 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
         <div className="h-24 w-96">
           <Logo />
         </div>
-        {teamNumber !== 2713 && (
+        {teamNumber !== 1708 && (
           <>
             <Heart className="text-primary size-8 fill-primary" />
             <span className="text-2xl text-primary font-rhr-ns">
