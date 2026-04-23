ce54b788b1c327f01b2e9617b977da75a8131a6f
Price <35711526+derk123c@users.noreply.github.com>
2026-04-22 22:26:28 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,6 +86,17 @@ module.exports = {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
+		theme: {
+  extend: {
+    colors: {
+      primary: 'var(--primary)',
+      primaryLight: 'var(--primary-light)',
+      primaryDark: 'var(--primary-dark)',
+      card: 'var(--bg-card)',
+      border: 'var(--border)',
+    }
+  }
+}
   	}
   },
   plugins: [require('@tailwindcss/forms'), require('tailwindcss-animate')],
